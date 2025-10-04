cb244350a5fda8d4d4d28abd1a25ad253b35b425
yingsu00 <yingsu00@outlook.com>
2025-10-03 17:54:15 -0700

    Add config and session property for exchange checksum
    Add exchange.checksum-enabled config property and
    native_exchange_checksum session property. The default value is false,
    meaning crc32 checksum computation is disabled by default.

--- presto-native-execution/presto_cpp/main/common/Configs.cpp
+++ presto-native-execution/presto_cpp/main/common/Configs.cpp
@@ -174,6 +174,7 @@ SystemConfig::SystemConfig() {
           NONE_PROP(kHttpsClientCaFile),
           NUM_PROP(kExchangeHttpClientNumIoThreadsHwMultiplier, 1.0),
           NUM_PROP(kExchangeHttpClientNumCpuThreadsHwMultiplier, 1.0),
+          BOOL_PROP(kExchangeChecksumEnabled, false),
           NUM_PROP(kConnectorNumCpuThreadsHwMultiplier, 0.0),
           NUM_PROP(kConnectorNumIoThreadsHwMultiplier, 1.0),
           NUM_PROP(kDriverNumCpuThreadsHwMultiplier, 4.0),
@@ -593,6 +594,10 @@ double SystemConfig::exchangeHttpClientNumIoThreadsHwMultiplier() const {
 double SystemConfig::exchangeHttpClientNumCpuThreadsHwMultiplier() const {
   return optionalProperty<double>(kExchangeHttpClientNumCpuThreadsHwMultiplier)
       .value();
+}
+
+bool SystemConfig::exchangeChecksumEnabled() const {
+  return optionalProperty<bool>(kExchangeChecksumEnabled).value();
 }
 
 double SystemConfig::connectorNumCpuThreadsHwMultiplier() const {
--- presto-native-execution/presto_cpp/main/properties/session/SessionProperties.h
+++ presto-native-execution/presto_cpp/main/properties/session/SessionProperties.h
@@ -425,6 +425,8 @@ class SessionProperties : public SessionPropertiesProvider {
   static constexpr const char* kAggregationMemoryCompactionReclaimEnabled =
       "native_aggregation_memory_compaction_reclaim_enabled";
 
+  static constexpr const char* kExchangeChecksum = "native_exchange_checksum";
+
   static SessionProperties* instance();
 
   SessionProperties();
